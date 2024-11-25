00b46e414faa87a0639bff8f1a2df396f54905d3
Mendez-Rafaela <coachdigitaliir@gmail.com>
2024-11-25 20:57:22 -0300

    function

---
+++ src/exercicios.js/function.js
@@ -0,0 +1,25 @@
+// função estática
+// function Salve() {
+//     return console.log('salve, sou uma função estática')
+// }
+
+// Salve()
+
+// function Saudacao() {
+//     return "Olá, Mundo!"
+// }
+
+// console.log(Saudacao)
+
+// function Somar() {
+//     return 2 + 2
+// }
+
+// console.log(Somar())
+
+// função Dinâmica
+function DynaSomar (a, b) {
+    return a + b
+}
+
+console.log(DynaSomar(10, 200))
