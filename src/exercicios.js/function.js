4016a225da0f35b8a820c6e38986674f69f838a1
Mendez-Rafaela <coachdigitaliir@gmail.com>
2024-11-25 21:37:01 -0300

    criei funções

--- src/exercicios.js/function.js
+++ src/exercicios.js/function.js
@@ -18,8 +18,29 @@
 // console.log(Somar())
 
 // função Dinâmica
-function DynaSomar (a, b) {
-    return a + b
+// function DynaSomar (a, b) {
+//     return a + b
+// }
+
+// console.log(DynaSomar(10, 200))
+
+// function NumberX(num) {
+//     if(num % 2 == 0) {
+//         return `${num} eu sou par`
+//     } else {
+//         return num + "Eu sou impar"
+//     }
+// }
+
+// console.log(NumberX(4))
+
+function ShortName (fullName) {
+    const splitName = fullName.split(' ')
+    const [firstName, ...rest] = splitName
+    const lastName = rest[rest.length -1]
+    
+    return console.log(`${firstName} ${lastName}`)
 }
 
-console.log(DynaSomar(10, 200))
+ShortName('Maria Jose dos Santos')
+
