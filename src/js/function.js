40eef4505dff5ed8be0c30f772aa516335ae4b6b
Mendez-Rafaela <coachdigitaliir@gmail.com>
2024-11-25 22:15:49 -0300

    criei os barato de função

--- src/js/function.js
+++ src/js/function.js
@@ -1,46 +1,91 @@
+// FUNÇÃO DECLARATIVA
+
 // função estática
 // function Salve() {
-//     return console.log('salve, sou uma função estática')
+//      return console.log('salve, sou uma função estática')
 // }
 
 // Salve()
 
-// function Saudacao() {
-//     return "Olá, Mundo!"
-// }
+ function Saudacao() {
+     return "Olá, Mundo!"
+ }
 
-// console.log(Saudacao)
+ console.log(Saudacao)
 
-// function Somar() {
-//     return 2 + 2
-// }
+ function Somar() {
+     return 2 + 2
+ }
 
-// console.log(Somar())
+ console.log(Somar())
 
 // função Dinâmica
-// function DynaSomar (a, b) {
-//     return a + b
-// }
+ function DynaSomar (a, b) {
+     return a + b
+ }
 
-// console.log(DynaSomar(10, 200))
+ console.log(DynaSomar(10, 200))
 
-// function NumberX(num) {
-//     if(num % 2 == 0) {
-//         return `${num} eu sou par`
-//     } else {
-//         return num + "Eu sou impar"
-//     }
-// }
+ function NumberX(num) {
+     if(num % 2 == 0) {
+         return `${num} eu sou par`
+     } else {
+         return num + "Eu sou impar"
+     }
+ }
 
-// console.log(NumberX(4))
+console.log(NumberX(4))
 
-function ShortName (fullName) {
-    const splitName = fullName.split(' ')
-    const [firstName, ...rest] = splitName
-    const lastName = rest[rest.length -1]
+ function ShortName (fullName) {
+     const splitName = fullName.split(' ')
+     const [firstName, ...rest] = splitName
+     const lastName = rest[rest.length -1]
     
-    return console.log(`${firstName} ${lastName}`)
+     return console.log(`${firstName} ${lastName}`)
+ }
+
+ ShortName('Maria Jose dos Santos')
+
+ const name = 'Joao Doria Bosla'
+ const transform = name.split(' ')
+
+ const [first, second, third]
+
+ const [first, ...rest] = transform
+
+console.table(transform)
+ console.log(first)
+ console.log(second)
+ console.log(rest)
+
+
+ function ResponseBool(bool) {
+    if(bool === true) {
+         return 'yes'
+     } else {
+         return 'no'
+     }
+ }
+
+
+// ternary 
+ function ResponseBool(bool) {
+     return bool ? 'Yes' : 'No'
+ }
+
+ console.log(ResponseBool(true))
+
+
+
+// FUNÇÕES ANÔNIMA
+
+const exampleAnonimo = function () {
+    console.log('Eu sou uma função anonima')
 }
+console.log(exampleAnonimo())
 
-ShortName('Maria Jose dos Santos')
-
+//arrow
+const arrowFunction = () => {
+    return console.log('Sou uma arrow function')
+}
+arrowFunction()
