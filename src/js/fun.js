d1a7b2a6532a19eb9e2531ba5f514a05e07b6766
Mendez-Rafaela <coachdigitaliir@gmail.com>
2024-11-27 19:23:22 -0300

    criei fun.js

---
+++ src/js/fun.js
@@ -0,0 +1,7 @@
+const fullname = 'Lucas do Espirito Santo'
+
+console.log(fullname.length, 'conta caracteres')
+
+const fruits = ['maça', 'pessego', 'uva']
+console.log(fruits.length, 'array')
+console.table(fruits)
